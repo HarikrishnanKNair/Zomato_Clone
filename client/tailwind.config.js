5e18b3a31a2c21f7091271122721a322223fdd8e
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-11 18:21:51 +0530

    add navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        zomato: {
+          50: '#ffe4e9',
+          100: '#feb6c0',
+          200: '#f88797',
+          300: '#f3586d',
+          400: '#ef2b44',
+          500: '#d6132b',
+          600: '#a70c21',
+          700: '#780617',
+          800: '#4a010c',
+          900: '#1f0003',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
