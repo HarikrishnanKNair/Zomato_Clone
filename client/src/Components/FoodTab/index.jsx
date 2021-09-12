37e19d3956cc22539e12d6b6264a17d8b4c90874
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-12 11:31:12 +0530

    add active tab to mobile screen

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { IoFastFoodOutline } from "react-icons/io5";
 import { BiDrink } from "react-icons/bi";
 import { GiMedicines } from "react-icons/gi";
 
 const MobileTabs = () => {
+    const [allTypes, setAllTypes ] = useState([
+        {
+            id: `delivery`,
+            icon: <RiShoppingBag3Line />,
+            name: "Delivery",
+            isActive: false,
+        },
+        {
+            id: `dining`,
+            icon: <IoFastFoodOutline />,
+            name: "Dining Out",
+            isActive: false,
+        },
+        {
+            id: `night`,
+            icon: <BiDrink />,
+            name: "Nightlife",
+            isActive: false,
+        },
+        {
+            id: `nutri`,
+            icon: <GiMedicines />,
+            name: "Nutrition",
+            isActive: false,
+        },
+    ]);
+    const { type } = useParams();
+
     return (
         <>
             <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex md:justify-evenly items-center justify-between text-gray-600 border ">
-                <div className="flex flex-col items-center text-xl">
-                    <RiShoppingBag3Line />
-                    <h5 className="text-sm">Delivery</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <IoFastFoodOutline />
-                    <h5 className="text-sm">Dining out</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <BiDrink />
-                    <h5 className="text-sm">Night life</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <GiMedicines />
-                    <h5 className="text-sm">Nutrition</h5>
-                </div>
+                {allTypes.map((items) => (
+                    <Link to={`/${items.id}`}>
+                        <div className={type === items.id ? "flex flex-col relative items-center text-xl text-zomato-400": "flex flex-col items-center text-xl"}>
+                            <div className={type === items.id && "absolute -top-3 w-full h-2 border-t-2 border-zomato-400"}/>
+                                {items.icon}
+                            <h5 className="text-sm">{items.name}</h5>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </>
     );
