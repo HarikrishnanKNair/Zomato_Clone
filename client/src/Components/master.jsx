4b77fb8fb3e7b6cb973fa71fc29b2b8d0e636369
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-13 22:55:54 +0530

    add night life

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -4,16 +4,17 @@ import { useParams } from 'react-router';
 // Components
 import Delivery from './Delivery';
 import Dining from './Dining';
+import NightLife from './NightLife';
 
 const Master = () => {
     const { type } = useParams();
     return (
         <>
-        <div className="my-5">
-            {type === "delivery" && <Delivery /> }
-            {type === "dining" && <Dining />} 
-        </div>
-           
+            <div className="my-5">
+                {type === "delivery" && <Delivery /> }
+                {type === "dining" && <Dining />}
+                {type === "night" && <NightLife />}
+            </div>
         </>
     );
 };
