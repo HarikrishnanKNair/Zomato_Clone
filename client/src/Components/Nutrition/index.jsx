ae71a419ec9afd0ef0a93230d3b65c301ad43e57
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-13 23:34:39 +0530

    [pending] add nutrition page

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Components
+import NutritionCarousal from './NutritionCarousal';
+import NutritionCarousalCard from './NutritionCarousalCard';
+
+const Nutrition = () => {
+    return (
+        <div>
+            <NutritionCarousal />
+        </div>
+    );
+};
+
+export default Nutrition;
