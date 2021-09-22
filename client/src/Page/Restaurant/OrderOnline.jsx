fe27560f49f287d9ba2f71611e052058ad9d5024
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-22 19:51:56 +0530

    add food list

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -6,27 +6,47 @@ import { BiTimeFive } from "react-icons/bi";
 import FloatMenuBtn from "../../Components/restaurant/Order-Online/FloatMenuBtn";
 import MenuListContainer from "../../Components/restaurant/Order-Online/MenuListContainer";
 import FoodItem from "../../Components/restaurant/Order-Online/FoodItem";
+import FoodList from "../../Components/restaurant/Order-Online/FoodList";
 
 const OrderOnline = () => {
     return (
         <>
-            <div className="w-full flex">
+            <div className="w-full h-screen flex">
                 <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
                     <MenuListContainer />
                     <MenuListContainer />
                 </aside>
-                <div className="w-full md:w-3/4">
-                    <h2 className="text-xl font-semibold">Order Online</h2>
-                    <h4 className="flex items-center gap-2 fomt-light text-gray-500">
-                        <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
-                    </h4>
-                    <section>
-                        <FoodItem 
-                            image="https://b.zmtcdn.com/data/dish_photos/df2/fdcba915279cc1f7bd53422584b94df2.jpg"
-                            price="1000"
-                            rating="3"
-                            description="Dig in this mildly spiced aromatic dish, every long grain of rice is infused with the flavours of mutton, ghee and fried onions. Served with Sala & Raita"
-                            title="Hyderabadi Dum Mutton Biryani"
+                <div className="w-full px-3 md:w-3/4">
+                    <div className="pl-3 mb-4 ">
+                        <h2 className="text-xl font-semibold">Order Online</h2>
+                        <h4 className="flex items-center gap-2 fomt-light text-gray-500">
+                            <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+                        </h4>
+                    </div>
+                    <section className="flex h-screen overflow-y-scroll flex-col gap-3 md:gap-5">
+                        <FoodList 
+                            title="Recommended"
+                            items={[
+                                {
+                                    price:"1000",
+                                    rating: 3,
+                                    description:"Dig in this mildly spiced aromatic dish, every long grain of rice is infused with the flavours of mutton, ghee and fried onions. Served with Sala & Raita",
+                                    title:"Hyderabadi Dum Mutton Biryani",
+                                    image:"https://b.zmtcdn.com/data/dish_photos/df2/fdcba915279cc1f7bd53422584b94df2.jpg",
+                                },
+                            ]}
+                        />
+                        <FoodList 
+                            title="Soups"
+                            items={[
+                                {
+                                    price:"1000",
+                                    rating: 3,
+                                    description:"Dig in this mildly spiced aromatic dish, every long grain of rice is infused with the flavours of mutton, ghee and fried onions. Served with Sala & Raita",
+                                    title:"Hyderabadi Dum Mutton Biryani",
+                                    image:"https://b.zmtcdn.com/data/dish_photos/df2/fdcba915279cc1f7bd53422584b94df2.jpg",
+                                },
+                            ]}
                         />
                     </section>
                 </div>
