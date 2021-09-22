bfce2702d7a1354ca5329b34760d09eb39c9c254
HarikrishnanKNair <harikrishnan2112003@gmail.com>
2021-09-22 17:46:49 +0530

    add order online menu

--- client/src/Components/restaurant/Order-Online/MenuListContainer.jsx
+++ client/src/Components/restaurant/Order-Online/MenuListContainer.jsx
@@ -21,54 +21,6 @@ const MenuListContainer = (props) => {
                     onClickHandler={onClickHandler}
                     isActive={selected === "Recommended"}
                 />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
-                <MenuCategory
-                    name="Recommended"
-                    items={["",""]}
-                    onClickHandler={onClickHandler}
-                    isActive={selected === "Recommended"}
-                />
             </div>
         </>
     );
